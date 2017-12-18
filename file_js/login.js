9fb0eb252129e178af389f3d28f4ad94b97ec40b
ciazhar <ciazhar.id@gmail.com>
2017-12-19 00:14:58 +0700

    implement login

---
+++ file_js/login.js
@@ -0,0 +1,22 @@
+var app = angular.module('indexApp',[]);
+
+app.controller('indexController',function ($scope,$window) {
+	
+	var urlBuy = "../file_coding/buy.html";
+	
+	$scope.doLogin = function() {
+		if($scope.login.email == null){
+			alert('email tidak valid')
+		}
+		else if($scope.login.password == null){
+			alert('password Kosong')
+		}
+		else if ($scope.login.email != 'ciazhar.id@gmail.com' && $scope.login.password!= '123') {
+			alert('email & password salah');
+		}
+		else{
+			$window.location.href=urlBuy;
+		}	
+	}
+
+})
